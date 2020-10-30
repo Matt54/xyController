b7130e0a0fab1cd40bd7fc260046ab19e5ee6486
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2020-10-30 18:14:25 -0500

    Initial Commit

---
+++ xyController/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xyController
+//
+//  Created by Matt Pfeiffer on 10/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xyController/xyControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  xyControllerApp.swift
+//  xyController
+//
+//  Created by Matt Pfeiffer on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct xyControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
